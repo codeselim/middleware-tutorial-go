153a5364d5ab9ea65a2086ad584b0679f25ed2e6
Salim Saber <selimabisaber@gmail.com>
2016-07-20 11:57:49 +0200

    create an http server

--- README.md
+++ README.md
@@ -2,6 +2,11 @@
 
 To install the project `go get github.com/codeselim/middleware-tutorial-go`
 
+## Step 2
+The main.go file creates an http server listening on port `8080`
+The server handles requests matching the path `"/"`  line 26:  `http.HandleFunc("/", ...`
+The servers is bound on the port 8080, listens and serves requests. line 29: `http.ListenAndServe(...`  
+
 ## Step: 1
 The `main.go` file initializes the application and prints a simple greeting string in the console.
  
--- main.go
+++ main.go
@@ -2,13 +2,19 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
 	"time"
 )
 
-func sayHello(name string) (string, string){
-	greeting := fmt.Sprintf("Hello %s", name)
+const (
+	port = "8080"
+)
+
+// handler function has the handler type signature
+func sayHello(w http.ResponseWriter, r *http.Request) {
 	timeNow := time.Now().Format(time.RFC850)
-	return greeting, timeNow
+	fmt.Fprintf(w, "Hello, now it is the %s", timeNow)
 }
 
 // init() is always called, regardless if there's main function or not, so if you import a package that has an init function, it will be executed.
@@ -16,8 +22,10 @@ func init() {
 	fmt.Println("Initializing...")
 }
 
-func main(){
-	greetingString, timeNowString := sayHello("Go")
-	fmt.Println(fmt.Sprintf("Main says: %s, time: %s", greetingString, timeNowString))
+func main() {
+	http.HandleFunc("/", sayHello)
+	fmt.Println("Binding http server on port: " + port)
+	log.Fatal(
+		http.ListenAndServe(":"+port, nil),
+	)
 }
-
